01f757b0a5f5d1e8e5374d0c99fdc396d17d2e6f
Sturke <burke.stanion@gmail.com>
2024-09-20 18:26:07 -0500

    add .nojekyll for gh-pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="https://kit.svelte.dev" target="_blank">kit.svelte.dev</a> to read the documentation</p>
 <h3>This is a test</h3>
-<p>This <a href="https://kit.svelte.dev/docs/project-structure">page</a> attempts to 
+<p>This <a href="https://kit.svelte.dev/docs/project-structure" target="_blank">page</a> attempts to 
 show how to structure your SvelteKit site components and pages.</p>
 <p><a href="/landing">Go to landing</a></p>
