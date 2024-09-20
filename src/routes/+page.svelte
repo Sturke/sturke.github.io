e973ef48f82212edc96c1c493a139a010630334d
Sturke <burke.stanion@gmail.com>
2024-09-20 15:04:55 -0500

    Bump vite from 5.4.2 to 5.4.6

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <h3>This is a test</h3>
-<a href="/landing.svelte">Go to landing</a>
+<p>This <a href="https://kit.svelte.dev/docs/project-structure">page</a> attempts to 
+show how to structure your SvelteKit site components and pages.</p>
+<p><a href="/landing">Go to landing</a></p>
