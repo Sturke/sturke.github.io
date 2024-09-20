85c48b9c70c0be6398a370b1aa56b6a899e5933e
Sturke <burke.stanion@gmail.com>
2024-09-20 09:49:33 -0500

    Add 404 and github config to svelte.config.js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h3>This is a test</h3>
+<a href="/landing.svelte">Go to landing</a>
---
+++ src/routes/landing.svelte
@@ -0,0 +1,2 @@
+<h1>Landing</h1>
+<a href="/+page">Go back to test</a>
