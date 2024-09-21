2b0fb2bfdeff2d609f218465e913e7ca7369186a
Sturke <burke.stanion@gmail.com>
2024-09-21 10:44:39 -0500

    build header component

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,14 +1,12 @@
 
-
-
 <svelte:head>
-    <title>Home Page</title>
+    <title>About Page</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>About Page</h1>
 <p>Visit <a href="https://kit.svelte.dev" target="_blank">kit.svelte.dev</a> to read the documentation</p>
-<h3>Hello World</h3>
-<p>This is a test<a href="/about">Lets Link to About Page</a></p>
+<h3>This is About linking two pages.</h3>
+<p>This is a test<a href="/">Lets Link to Homepage</a></p>
 <p>This <a href="https://kit.svelte.dev/docs/project-structure" target="_blank">page</a> attempts to 
 show how to structure your SvelteKit site components and pages.</p>
 <p><a href="https://kit.svelte.dev/docs/routing" target="_blank">Continue Learning SvelteKit Routing</a></p>
