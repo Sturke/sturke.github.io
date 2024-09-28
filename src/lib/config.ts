9d8ad08b16b97b7c8ab0a10f0f8742cf98346a16
Sturke <burke.stanion@gmail.com>
2024-09-28 10:04:39 -0500

    Add config.ts to lib

---
+++ src/lib/config.ts
@@ -0,0 +1,6 @@
+import { dev } from "$app/environment";
+
+export const title = "Sturke";
+export cost description = "Sturke Portfolio Project";
+
+export const url = dev ? "http://localhost:5173" : "https://sturke.com";
