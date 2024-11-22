429ade105d0dbd333c7e314a5cbf0b4205db906e
Sturke <burke.stanion@gmail.com>
2024-11-22 16:59:03 -0600

    Updates to routes

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,8 +1,46 @@
-<nav>
 
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/landing">Landing</a>
-    
+
+
+<nav>
+    <ul class="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/portfolio">Portfolio</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/blog">Blog</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/landing">Landing</a></li>
+        
+
+    </ul>
+    <!-- Theme -->
+    <button>Toggle</button>
 </nav>
+
+
+<style>
+	nav {
+		padding-block: var(--size-7);
+	}
+
+	.nav-links {
+		margin-block: var(--size-7);
+	}
+
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+
+	@media (min-width: 768px) {
+		nav {
+			display: flex;
+			justify-content: space-between;
+		}
+
+		.nav-links {
+			display: flex;
+			gap: var(--size-7);
+			margin-block: 0;
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,41 @@
-<script>
-    import Header from "$lib/components/header.svelte";
+<script lang="ts">
+    import Header from "$lib/components/header.svelte"
+    import Footer from "$lib/components/footer.svelte"
+
+    
+    ;
 </script>
 
-<Header />
-<main><slot />
-</main>
+<div class="layout">
+    <!-- Header -->
+      <Header />
+  
+      <main>
+          <!-- Black hole for other content -->
+          <slot />
+      </main>
+  
+    <!-- Footer -->
+      <Footer />
+  </div>
+  
+  <style>
+      .layout {
+          height: 100%;
+          max-inline-size: 1440px;
+          display: grid;
+          grid-template-rows: auto 1fr auto;
+          margin-inline: auto;
+          padding-inline: var(--size-7);
+      }
+  
+      main {
+          padding-block: var(--size-9);
+      }
+  
+      @media (min-width: 1440px) {
+          .layout {
+              padding-inline: 0;
+          }
+      }
+  </style>
